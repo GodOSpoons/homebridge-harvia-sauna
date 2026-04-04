49ea98e9f075488dcd57c8cf447cefd61332a430
jos3phburns-afk <jos3phburns@gmail.com>
2026-04-04 14:30:33 -0400

    Switch Updates

--- src/accessories/SwitchAccessory.ts
+++ src/accessories/SwitchAccessory.ts
@@ -1,12 +1,12 @@
 import { HarviaDevice, DeviceStateSubscriber } from '../HarviaDevice';
-import { PlatformAccessory, Logger, CharacteristicValue, API, Characteristic } from 'homebridge';
+import { PlatformAccessory, Logger, CharacteristicValue, API, HAP } from 'homebridge';
 import type { Service } from 'homebridge';
 
 export type SwitchType = 'power' | 'light' | 'fan' | 'steamer';
 
 export class SwitchAccessory implements DeviceStateSubscriber {
   private readonly service: Service;
-  private readonly Characteristic: typeof Characteristic;
+  private readonly Characteristic: HAP['Characteristic'];
   private readonly displayName: string;
 
   constructor(
@@ -24,14 +24,12 @@ export class SwitchAccessory implements DeviceStateSubscriber {
       .setCharacteristic(Characteristic.Manufacturer, 'Harvia')
       .setCharacteristic(Characteristic.Model, 'Xenio WiFi')
       .setCharacteristic(Characteristic.SerialNumber, `${device.id}-${type}`);
-
     this.service
       .getCharacteristic(Characteristic.On)
       .onGet(() => this.getCurrentState())
       .onSet(async (value: CharacteristicValue) => {
         await this.setState(value === true || value === 1);
       });
-
     this.device.subscribe(this);
   }
 
@@ -41,31 +39,19 @@ export class SwitchAccessory implements DeviceStateSubscriber {
 
   private getCurrentState(): boolean {
     switch (this.type) {
-      case 'power':
-        return this.device.active;
-      case 'light':
-        return this.device.lightsOn;
-      case 'fan':
-        return this.device.fanOn;
-      case 'steamer':
-        return this.device.steamOn;
+      case 'power': return this.device.active;
+      case 'light': return this.device.lightsOn;
+      case 'fan': return this.device.fanOn;
+      case 'steamer': return this.device.steamOn;
     }
   }
 
   private async setState(value: boolean): Promise<void> {
     switch (this.type) {
-      case 'power':
-        await this.device.setActive(value);
-        break;
-      case 'light':
-        await this.device.setLight(value);
-        break;
-      case 'fan':
-        await this.device.setFan(value);
-        break;
-      case 'steamer':
-        await this.device.setSteamer(value);
-        break;
+      case 'power': await this.device.setActive(value); break;
+      case 'light': await this.device.setLight(value); break;
+      case 'fan': await this.device.setFan(value); break;
+      case 'steamer': await this.device.setSteamer(value); break;
     }
   }
 }
